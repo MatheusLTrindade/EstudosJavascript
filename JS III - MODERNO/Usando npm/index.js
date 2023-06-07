bc81738774470cc7c1b708eb0b1ca0d39313a42c
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-06-07 06:17:21 -0300

    add Usando npm

---
+++ JS III - MODERNO/Usando npm/index.js
@@ -0,0 +1,5 @@
+const lodash = require("lodash")
+
+console.log(lodash.isArray([]))
+console.log(lodash.kebabCase("Matheus Lopes Trindade")) // kebab-case
+console.log(lodash.camelCase("Matheus Lopes Trindade")) // camelCase
