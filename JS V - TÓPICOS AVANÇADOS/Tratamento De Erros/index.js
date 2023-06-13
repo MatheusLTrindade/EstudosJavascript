7f9531ecf0e358863e1d1de7e56e57a7ecbe1a0b
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-06-13 18:11:17 -0300

    add Tratamento de erros (try, catch e finally)

---
+++ JS V - TÓPICOS AVANÇADOS/Tratamento De Erros/index.js
@@ -0,0 +1,24 @@
+function sum(a, b) {
+    const firstNumber = Number(a)
+    const secoundNumber = Number(b)
+
+    if (isNaN(firstNumber) || isNaN(secoundNumber)) {
+        throw new Error('arguments must be two numbers')
+    }
+
+    return firstNumber + secoundNumber
+}
+
+try {
+    console.log(sum(2, 9))
+    console.log(sum(true, 14))
+    console.log(sum(undefined, 22))
+    console.log(sum(18, '0'))
+    console.log(sum(39, null))
+    console.log(sum(13, 'zero'))
+} catch (error) {
+    console.log('An error ocurred!')
+    console.log(error.message)
+} finally {
+    console.log('Calculations finished!')
+}
