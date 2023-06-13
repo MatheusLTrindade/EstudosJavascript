96e56343e41ee672043b3e9fbd4434df30cd244d
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-06-12 23:42:35 -0300

    add Encapsulamento

---
+++ JS IV - POO/Encapsulamento/Account.js
@@ -0,0 +1,30 @@
+class Account {
+    #password
+    #balance = 0
+
+    constructor(user) {
+        this.email = user.email
+        this.#password = user.password
+    }
+    getBalance(email, password) {
+      if (this.#authenticate(email, password)) {
+        return this.#balance
+      }
+      return null
+    }
+
+    #authenticate(email, password) {
+        return this.email === email && this.#password === password
+    }
+}
+
+const user = {
+    email: "exemple@email.com",
+    password: "123456"
+}
+
+const myAccount = new Account(user)
+
+console.log(myAccount)
+console.log(myAccount.getBalance("exemple@email.com", "123"))
+console.log(myAccount.getBalance("exemple@email.com", "123456"))
